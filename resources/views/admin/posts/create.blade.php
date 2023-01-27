cf8ba363a13f3bc1ba4bf55f6bf90ac7cde23177
João Felipe <joaofelipecos@gmail.com>
2023-01-27 17:18:23 -0300

    Classes tailwind, projeto finalizado

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,14 +1,13 @@
-@extends ('admin.layouts.app')
+@extends('admin.layouts.app')
 
-@section ('title', 'Criar novo Post')
+@section('title', 'Criar Novo Post')
 
 @section('content')
-    <h1 class="text-center text-3x1 uppercase font-black my-4"> Novo post</h1>
+    <h1 class="text-center text-3xl uppercase font-black my-4">Novo Post</h1>
 
     <div class="w-11/12 p-12 bg-white sm:w-8/12 md:w-1/2 lg:w-5/12 mx-auto">
-        <form action="{{ route('posts.store')}}" method="post" enctype="multipart/form-data">
-        @include('admin.posts._partials.form')
+        <form action="{{ route('posts.store') }}" method="post" enctype="multipart/form-data">
+            @include('admin.posts._partials.form')
         </form>
     </div>
-
 @endsection
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -1,15 +1,14 @@
-@extends ('admin.layouts.app')
+@extends('admin.layouts.app')
 
-@section('title', "Editar Post {$post->title}")
+@section('title', "Editar o Post {$post->title}")
 
 @section('content')
-    <h1> Editar Post:<strong> {{ $post->title }} </strong> </h1>
+    <h1 class="text-center text-3xl uppercase font-black my-4">Editar o Post <strong>{{ $post->title }}</strong></h1>
 
-    <form action="{{ route('posts.update', $post->id)}}" method="post" enctype="multipart/form-data">
-
+    <div class="w-11/12 p-12 bg-white sm:w-8/12 md:w-1/2 lg:w-5/12 mx-auto">
+    <form action="{{ route('posts.update', $post->id) }}" method="post" enctype="multipart/form-data">
         @method('put')
         @include('admin.posts._partials.form')
-
     </form>
-
+    </div>
 @endsection
