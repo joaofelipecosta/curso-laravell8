c13a212dc70d91b93ab03da421966be321af4c0c
João Felipe <joaofelipecos@gmail.com>
2023-01-27 10:23:27 -0300

    Install dependencies

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,14 @@ Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
 Route::post('/posts',[PostController::class, 'store'])->name('posts.store');
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+require __DIR__.'/auth.php';
